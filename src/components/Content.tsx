9e4621fe42e7411845a55fbf214d21ce85d8bf5b
Igor <joao.igor@hp.com>
2021-11-11 08:49:32 -0300

    Finish chanllenger

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
-}
+import { GenreResponseProps } from "../schemas/Genrer";
+import { MovieProps } from "../schemas/Movies";
+import { MovieCard } from "./MovieCard";
+
+type ContentProps = {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+};
+
+const Content = ({ movies, selectedGenre }: ContentProps) => {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+export { Content };
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
-}
+import { GenreResponseProps } from "../schemas/Genrer";
+import { Button } from "./Button";
+
+type SideBar = {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  handleClickButton: { (id: number): void };
+};
+
+const SideBar = ({ selectedGenreId, genres, handleClickButton }: SideBar) => {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export { SideBar };
